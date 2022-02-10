96f16cce3e6522143425f56dd5c27fc4fbaefc1f
Thorsten Buss <buss@thphys.uni-heidelberg.de>
2022-02-10 14:25:13 +0100

    add MyDataLoader

---
+++ src/myDataLoader.py
@@ -0,0 +1,54 @@
+import math
+import torch
+from torch.distributions import Beta
+
+class MyDataLoader:
+
+    data: torch.Tensor
+    batch_size: int
+    drop_last: bool
+    shuffle: bool
+
+    def __init__(self, data: torch.Tensor, cond: torch.Tensor, batch_size: int, drop_last:bool=False, shuffle:bool=True):
+        self.data = data
+        self.cond = cond
+        self.batch_size = batch_size
+        self.drop_last = drop_last
+        self.shuffle = shuffle
+        self.deta_distribution = Beta(torch.tensor([3.], device=data.device), torch.tensor([3.], device=data.device))
+
+        if self.drop_last:
+            self.max_batch = len(self.data) // self.batch_size
+        else:
+            self.max_batch = math.ceil(len(self.data) / self.batch_size)
+  
+        self.initialize()
+
+    def add_noise(self, input):
+        noise = self.deta_distribution.sample(input.shape)*8e-7
+        return input - noise[...,0]
+
+    def __len__(self) -> int:
+        return self.max_batch
+
+    def __iter__(self):
+        return self
+
+    def initialize(self):
+        if self.shuffle:
+            self.index = torch.randperm(len(self.data), device=self.data.device)
+        else:
+            self.index = torch.arange(len(self.data), device=self.data.device)
+        self.batch = 0
+
+    def __next__(self):
+        if self.batch >= self.max_batch:
+            self.initialize()
+            raise StopIteration
+        first = self.batch*self.batch_size
+        last = min(first+self.batch_size, len(self.data))
+        idx = self.index[first:last]
+        self.batch += 1
+        data = torch.clone(self.add_noise(self.data[idx]))
+        cond = torch.clone(self.cond[idx])
+        return data, cond
