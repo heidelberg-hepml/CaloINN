ea119a999650a3cb9ed3672e39fcab78fab10174
Thorsten Buss <buss@thphys.uni-heidelberg.de>
2022-02-13 13:40:18 +0100

    save results generate samples

--- src/model.py
+++ src/model.py
@@ -67,41 +67,36 @@ class LogTransformation(fm.InvertibleModule):
     def output_dims(self, input_dims):
         return input_dims
 
-
-class CondNormINN(ff.GraphINN):
-    def __init__(self, node_list, pre_subnet=None):
-        super().__init__(node_list)
-        self.pre_subnet = pre_subnet
-
-    def forward(self, x, c, rev=False, jac=True):
-        c_norm = torch.log(c)
-        if self.pre_subnet is not None:
-            c_norm = self.pre_subnet(c_norm)
-        return super().forward(x, c_norm, rev=rev, jac=jac)
-
-
 class CINN(nn.Module):
     """ Class to build, train and evaluate a cINN model """
 
-    def __init__(self, params, device):
+    def __init__(self, params, device, data):
         """ Initializes model class with run parameters and the torch device
         instance. """
         super(CINN, self).__init__()
         self.params = params
         self.device = device
+        self.num_dim = data.shape[1]
 
         self.verbose = params["verbose"]
         self.parton_norm_m = None
         self.bayesian = params.get("bayesian", False)
         self.alpha = params.get("alpha", 1e-6)
+        self.pre_subnet = None
         if self.bayesian:
             self.bayesian_layers = []
         self.losses = {"inn": []}
         if self.bayesian:
             self.losses.update({"kl": [], "total": []})
 
+        self.initialize_normalization(data)
+        self.define_model_architecture(self.num_dim)
+
     def forward(self, x, c, rev=False, jac=True):
-        return self.model(x, c, rev=rev, jac=jac)
+        c_norm = torch.log(c)
+        if self.pre_subnet is not None:
+            c_norm = self.pre_subnet(c_norm)
+        return self.model.forward(x, c_norm, rev=rev, jac=jac)
 
     def get_constructor_func(self, params):
         """ Returns a function that constructs a subnetwork with the given parameters """
@@ -209,50 +204,11 @@ class CINN(nn.Module):
         nodes.append(ff.OutputNode([nodes[-1].out0], name='out'))
         nodes.append(cond_node)
 
-        self.model = CondNormINN(nodes).to(self.device)
+        self.model = ff.GraphINN(nodes)
         self.params_trainable = list(filter(
                 lambda p: p.requires_grad, self.model.parameters()))
         n_trainable = sum(p.numel() for p in self.params_trainable)
         print(f"  Number of parameters: {n_trainable}", flush=True)
-
-    def set_optimizer(self, steps_per_epoch=1, no_training=False, params=None):
-        """ Initialize optimizer and learning rate scheduling """
-        if params is None:
-            params = self.params
-
-        self.optim = torch.optim.AdamW(
-            self.params_trainable,
-            lr = params.get("lr", 0.0002),
-            betas = params.get("betas", [0.9, 0.999]),
-            eps = params.get("eps", 1e-6),
-            weight_decay = params.get("weight_decay", 0.)
-        )
-
-        if no_training: return
-
-        self.lr_sched_mode = params.get("lr_scheduler", "reduce_on_plateau")
-        if self.lr_sched_mode == "step":
-            self.scheduler = torch.optim.lr_scheduler.StepLR(
-                self.optim,
-                step_size = params["lr_decay_epochs"],
-                gamma = params["lr_decay_factor"],
-            )
-        elif self.lr_sched_mode == "reduce_on_plateau":
-            self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-                self.optim,
-                factor = 0.4,
-                patience = 50,
-                cooldown = 100,
-                threshold = 5e-5,
-                threshold_mode = "rel",
-                verbose=True
-            )
-        elif self.lr_sched_mode == "one_cycle_lr":
-            self.scheduler = torch.optim.lr_scheduler.OneCycleLR(
-                self.optim,
-                params.get("max_lr", params["lr"]*10),
-                epochs = params.get("opt_epochs") or params["n_epochs"],
-                steps_per_epoch=steps_per_epoch)
 
     def set_bayesian_std_grad(self, requires_grad):
         for layer in self.bayesian_layers:
@@ -264,44 +220,13 @@ class CINN(nn.Module):
     def import_random_state(self, state):
         [layer.import_random_state(s) for layer, s in zip(self.bayesian_layers, state)]
 
-    # def save(self, epoch=""):
-    #     """ Save the model, its optimizer, losses, learning rates and the epoch """
-    #     if self.angle_enabled and self.angle_cond_mode == "one_hot":
-    #         angle_data = { "angle_options": self.angle_options }
-    #     else:
-    #         angle_data = {}
-
-    #     os.makedirs(self.doc.get_file("model", False), exist_ok=True)
-    #     torch.save({"opt": self.optim.state_dict(),
-    #                 "net": self.model.state_dict(),
-    #                 "losses": self.losses,
-    #                 "learning_rates": self.learning_rates,
-    #                 "epoch": self.epoch,
-    #                 **angle_data}, self.doc.get_file(f"model/model{epoch}", False))
-
-    # def load(self, epoch=""):
-    #     """ Load the model, its optimizer, losses, learning rates and the epoch """
-    #     name = self.doc.get_file(f"model/model{epoch}", False)
-    #     state_dicts = torch.load(name, map_location=self.device)
-    #     self.model.load_state_dict(state_dicts["net"])
-
-    #     self.losses = state_dicts.get("losses", {})
-    #     if isinstance(self.losses, list):
-    #         self.losses = { "inn": self.losses }
-    #     self.learning_rates = state_dicts.get("learning_rates", [])
-    #     self.epoch = state_dicts.get("epoch", 0)
-    #     self.optim.load_state_dict(state_dicts["opt"])
-    #     self.model.to(self.device)
-    #     if "angle_options" in state_dicts:
-    #         self.angle_options = state_dicts["angle_options"]
-
     def sample(self, num_pts, condition):
         z = torch.normal(0, 1, size=(num_pts*condition.shape[0], self.in_dim), device=self.device)
         c = condition.repeat(num_pts,1)
-        x, _ = self.model(z, c, rev=True)
+        x, _ = self.forward(z, c, rev=True)
         return x.reshape(num_pts, condition.shape[0], self.in_dim).permute(1,0,2)
 
     def log_prob(self, x, c):
-        z, log_jac_det = self.model(x, c, rev=False)
+        z, log_jac_det = self.forward(x, c, rev=False)
         log_prob = - 0.5*torch.sum(z**2, 1) + log_jac_det - z.shape[1]/2 * math.log(2*math.pi)
         return log_prob
--- src/trainer.py
+++ src/trainer.py
@@ -1,11 +1,11 @@
 import sys
+import os
 
 import torch
-
 import yaml
 from tqdm.auto import tqdm
 
-from data import get_loaders
+import data
 from model import CINN
 
 class Trainer:
@@ -14,7 +14,7 @@ class Trainer:
         self.params = params
         self.device = device
 
-        train_loader, test_loader = get_loaders(
+        train_loader, test_loader = data.get_loaders(
             params.get('data_path'),
             params.get('batch_size'),
             params.get('train_split', 0.8),
@@ -25,49 +25,129 @@ class Trainer:
 
         self.num_dim = train_loader.data.shape[1]
 
-    def train(self, inn):
+        model = CINN(params, device, self.train_loader.data)
+        self.model = model.to(device)
+        self.set_optimizer(steps_per_epoch=len(train_loader))
 
-        losses_train = []
-        losses_test = []
+        self.losses_train = []
+        self.losses_test = []
+        self.learning_rates = []
 
-        inn.initialize_normalization(self.train_loader.data)
-        inn.define_model_architecture(self.num_dim)
-        inn.set_optimizer(steps_per_epoch=len(self.train_loader))
+        os.makedirs(self.get_file(""), exist_ok=True)
 
-        for epoch in tqdm(range(1,self.params['n_epochs']+1), desc='Epoch', disable=not self.params.get('verbose', True)):
+    def train(self):
+
+        for epoch in tqdm(range(1,self.params['n_epochs']+1), desc='Epoch',
+                         disable=not self.params.get('verbose', True)):
+            self.epoch = epoch
             train_loss = 0
             test_loss = 0
 
-            inn.train()
+            self.model.train()
             for x, c in self.train_loader:
-                inn.optim.zero_grad()
-                loss = - torch.mean(inn.log_prob(x,c))
+                self.optim.zero_grad()
+                loss = - torch.mean(self.model.log_prob(x,c))
                 loss.backward()
-                inn.optim.step()
+                self.optim.step()
                 train_loss += loss.detach().cpu().numpy()*len(x)
-                inn.scheduler.step()
+                self.scheduler.step()
             train_loss /= len(self.train_loader.data)
 
-            inn.eval()
+            self.model.eval()
             with torch.no_grad():
                 for x, c in self.test_loader:
-                    loss = - torch.mean(inn.log_prob(x,c))
+                    loss = - torch.mean(self.model.log_prob(x,c))
                     test_loss += loss.detach().cpu().numpy()*len(x)
                 test_loss /= len(self.test_loader.data)
 
-                tqdm.write('')
-                tqdm.write(f'=== epoch {epoch} ===')
-                tqdm.write(f'inn loss (train): {train_loss}')
-                tqdm.write(f'inn loss (test): {test_loss}')
-                tqdm.write(f'lr: {inn.scheduler.get_last_lr()[0]}')
-                sys.stdout.flush()
-            
-            losses_train.append(train_loss)
-            losses_test.append(test_loss)
+            tqdm.write('')
+            tqdm.write(f'=== epoch {epoch} ===')
+            tqdm.write(f'inn loss (train): {train_loss}')
+            tqdm.write(f'inn loss (test): {test_loss}')
+            tqdm.write(f'lr: {self.scheduler.get_last_lr()[0]}')
+            sys.stdout.flush()
 
-        # if result_dir:
-        #     np.save(os.path.join(result_dir, 'losses_over_epochs_train.npy'), np.array(losses_over_epochs_train))
-        #     np.save(os.path.join(result_dir, 'losses_over_epochs_test.npy'), np.array(losses_over_epochs_test))
+            self.losses_train.append(train_loss)
+            self.losses_test.append(test_loss)
+            self.learning_rates.append(self.scheduler.get_last_lr()[0])
+
+    def set_optimizer(self, steps_per_epoch=1, no_training=False, params=None):
+        """ Initialize optimizer and learning rate scheduling """
+        if params is None:
+            params = self.params
+
+        self.optim = torch.optim.AdamW(
+            self.model.params_trainable,
+            lr = params.get("lr", 0.0002),
+            betas = params.get("betas", [0.9, 0.999]),
+            eps = params.get("eps", 1e-6),
+            weight_decay = params.get("weight_decay", 0.)
+        )
+
+        if no_training: return
+
+        self.lr_sched_mode = params.get("lr_scheduler", "reduce_on_plateau")
+        if self.lr_sched_mode == "step":
+            self.scheduler = torch.optim.lr_scheduler.StepLR(
+                self.optim,
+                step_size = params["lr_decay_epochs"],
+                gamma = params["lr_decay_factor"],
+            )
+        elif self.lr_sched_mode == "reduce_on_plateau":
+            self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                self.optim,
+                factor = 0.4,
+                patience = 50,
+                cooldown = 100,
+                threshold = 5e-5,
+                threshold_mode = "rel",
+                verbose=True
+            )
+        elif self.lr_sched_mode == "one_cycle_lr":
+            self.scheduler = torch.optim.lr_scheduler.OneCycleLR(
+                self.optim,
+                params.get("max_lr", params["lr"]*10),
+                epochs = params.get("opt_epochs") or params["n_epochs"],
+                steps_per_epoch=steps_per_epoch)
+
+    def save(self, epoch=""):
+        """ Save the model, its optimizer, losses, learning rates and the epoch """
+        torch.save({"opt": self.optim.state_dict(),
+                    "net": self.model.state_dict(),
+                    "losses": self.losses_test,
+                    "learning_rates": self.learning_rates,
+                    "epoch": self.epoch}, self.get_file(f"model{epoch}.pt"))
+
+    def load(self, epoch=""):
+        """ Load the model, its optimizer, losses, learning rates and the epoch """
+        name = self.get_file(f"model{epoch}.pt")
+        state_dicts = torch.load(name, map_location=self.device)
+        self.model.load_state_dict(state_dicts["net"])
+
+        self.losses_test = state_dicts.get("losses", {})
+        self.learning_rates = state_dicts.get("learning_rates", [])
+        self.epoch = state_dicts.get("epoch", 0)
+        self.optim.load_state_dict(state_dicts["opt"])
+        self.model.to(self.device)
+    
+    def get_file(self, name):
+        res_dir = self.params.get('res_dir', 'results')
+        return os.path.join(res_dir, name)
+    
+    def generate(self, num_samples, batch_size = 10000):
+        with torch.no_grad():
+            energies = 0.99*torch.rand((num_samples,1)) + 0.01
+            samples = torch.zeros((num_samples,1,self.num_dim))
+            for batch in tqdm(range((num_samples+batch_size-1)//batch_size), desc='Batch',
+                         disable=not self.params.get('verbose', True)):
+                    start = batch_size*batch
+                    stop = min(batch_size*(batch+1), num_samples)
+                    energies_l = energies[start:stop].to(self.device)
+                    samples[start:stop] = self.model.sample(1, energies_l).cpu()
+            samples = samples[:,0,...].cpu().numpy()
+            energies = energies.cpu().numpy()
+        data.save_data(self.get_file('samples.hdf5'), samples, energies)
+
 
 def main():
     if len(sys.argv)>=3:
@@ -80,8 +160,9 @@ def main():
     device = 'cuda:0' if use_cuda else 'cpu'
     print(device)
     trainer = Trainer(params, device)
-    inn = CINN(params, device)
-    trainer.train(inn)
+    trainer.train()
+    trainer.save()
+    trainer.generate(100000)
 
 if __name__=='__main__':
     main()    
