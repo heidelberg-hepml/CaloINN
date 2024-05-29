62b4256cee2d993638e3182c28c3f8f4d0ccd276
Florian Ernst <florian.ernst@cern.ch>
2024-05-29 15:14:26 +0200

    updated runcards

--- params/run.sh
+++
@@ -1,6 +0,0 @@
-cd ../src
-
-python3 ./main.py "../params/photons.yaml" -c
-# python3 ./main.py "../params/pions.yaml" -c
-# python3 ./main.py "../params/set2_KVAE.yaml" -c
-# python3 ./main_ecae.py "../params/set3_KVAE.yaml" -c
